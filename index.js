1f2b2e827ef6cd42c2bcec36c4634c5e1d30c41a
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2023-03-17 20:03:19 +0530

    appenChild problem in quizContainer

--- index.js
+++ index.js
@@ -54,23 +54,42 @@ const loadQuiz = async () => {
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
+
+
+  // for(const i of data){
+  //   const div = document.createElement('div');
+  //   div.innerHTML=`hello`;
+
+  //   quizContainer.appendChild(div)
+   
+  // }
+
+
+
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
 
   data.forEach((quiz, i) => {
-    quizContainer.innerHTML = `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
+
+    const div = document.createElement('div');
+ 
+
+    div.innerHTML = `<div class="m-3 py-3 px-4 shadow-sm rounded">
+      <div class="flex items-center">
+        <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+          ${i + 1}
+        </div>
+        <p class="text-gray-800 text-sm">${quiz.question}</p>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mt-5">
+        ${displayQuizOptions(quiz.options, i)}
+      </div>
+    </div>`;
+
+    quizContainer.appendChild(div)
+
   });
 };
 
